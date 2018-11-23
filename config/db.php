cfa4bebd08b3d7c6210be1a3c2aaf5f38a76f981
José Joaquín Garci <josejoaquin.garci@iesdonana.org>
2018-11-23 21:22:38 +0100

    Genera Generos y Peliculas

--- config/db.php
+++ config/db.php
@@ -22,9 +22,9 @@ if (($url = getenv('DATABASE_URL')) !== false) {
     // Configuración para entorno local:
     $host = 'localhost';
     $port = '5432';
-    $dbname = 'yiiaffinity';
-    $username = 'yiiaffinity';
-    $password = 'yiiaffinity';
+    $dbname = 'fa';
+    $username = 'fa';
+    $password = 'fa';
     $extra = [];
 }
 
